c4ef58ee67eac5a943175ccc4df53256384bbeda
Christopher Caysido <christopher.caysido@gmail.com>
2024-12-12 15:57:11 +0800

    accomplished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("")
+    const reversedStringArray = stringArray.toReversed()
+    const reverseString = reversedStringArray.join("")
+    return reverseString
 };
 
 // Do not edit below this line
