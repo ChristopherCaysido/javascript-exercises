a66a99630b7fb1ec55517995f750b28bbdc4c3d8
Christopher Caysido <christopher.caysido@gmail.com>
2024-12-13 14:00:35 +0800

    accomplished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...items) {
+    let finalArray
+    items.forEach((item)=>{
+        if(!arr.includes(item)){
+            finalArray = arr
+            return
+        }
+        while(arr.includes(item)){
+            const index = arr.indexOf(item)
+            arr.splice(index,1)
+        }
+        finalArray = arr
+    })
+    return finalArray
 };
+
+removeFromArray([1, 2, 3, 4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
